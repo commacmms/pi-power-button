2d2775914e16bfb044df8ebf927345a19b6474fd
RuiAlves <rui.alves@commacmms.com>
2022-06-27 15:58:11 +0800

    Update

--- listen-for-motion.py
+++ listen-for-motion.py
@@ -3,19 +3,28 @@
 #https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 import RPi.GPIO as GPIO
 import subprocess
+import keyboard
+import time
 
 GPIO.setmode(GPIO.BCM)
 #This is to initialize the PIR sensor (on pin 11, GPIO17)
 #Activate the internal pull up resistor on this pin.
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
 
 def button_callback(channel):
-	subprocess.Popen(['vcgencmd', 'display_power', '1'], stdout=subprocess.PIPE)
-	print("detected")
+	keyboard.press_and_release('a')
+	#Ligh up an LED:
+	GPIO.output(27, GPIO.HIGH)
+	#print("detected")
 
-GPIO.add_event_detect(17,GPIO.FALLING,callback=button_callback)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=button_callback)
 
-message = input("Press enter to quit\n\n")
+while 1:
+	time.sleep(3);
+	GPIO.output(27, GPIO.LOW)
+
+#message = input("Press enter to quit\n\n")
 
 GPIO.cleanup()
 
